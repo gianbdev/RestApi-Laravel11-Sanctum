586b6873746a3a44000db6375d55bbf4600a55fd
gianbdev <giancarlo.silva550@gmail.com>
2024-12-04 18:13:45 -0500

    add chamge | ohter entity drinks

---
+++ app/Repository/DrinkRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repository;
+
+class DrinkRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Services/DrinkService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class DrinkService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
